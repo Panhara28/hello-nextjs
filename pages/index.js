7e8ea12f26e8b4566e6baa557b8b5d02447ae006
Panhara28 <titpanhara@gmail.com>
2020-01-01 14:23:34 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Layout } from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href={ `/post?title=${props.title}` }>
+      <a>{ props.title }</a>
+    </Link>
+  </li>
+)
+
+const Index = () => (
+  <Layout>
+    <h1>Hello Next.js</h1>
+    <PostLink title="POST 1"/>
+    <PostLink title="POST 2"/>
+    <PostLink title="POST 3"/>
+  </Layout>
+);
+
+export default Index;
